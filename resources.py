86972297d618ed38d06aa595e34f00edca63d716
Justin Jagielski <jjagiels@udel.edu>
2019-05-20 16:48:18 -0400

    Actual merge

--- resources.py
+++ resources.py
@@ -5,15 +5,6 @@ from sqlalchemy import ForeignKey, Table
 from sqlalchemy.orm import relationship
 from base import Base
 
-<<<<<<< HEAD
-association_table = Table('resource_association', Base.metadata,
-                            Column('people', Integer, ForeignKey('people.pid')),
-                            Column('tile', Integer, ForeignKey('tile.pid')),
-                            Column('resources', Integer, ForeignKey('ownerID'))
-                            )
-=======
->>>>>>> justin_test1
-
 class Resources(Base):
     __tablename__ = 'resources'
 
